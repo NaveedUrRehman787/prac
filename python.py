215514477f019b7648b18ce69335eb516831e314
Naveed Ur Rehman <naveedurrehman787@gmail.com>
2022-11-11 15:10:09 +0500

    cool

--- python.py
+++ python.py
@@ -1,1 +1,7 @@
 print("Welcome to python in github folder")
+print("Wow! that's pretty coooooooooooool")
+# git process
+# git clone and url of the folder
+# git add and the file names
+# git commit -m "message" every time change in the file
+# git push   send the folder or files on the gihub server
